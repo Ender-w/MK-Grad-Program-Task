bc4982793a8c86aa0f0f4b1a4c8f5e4098c38455
Ender <danieldello@hotmail.com>
2019-01-22 13:06:42 +1030

    Project Upload

---
+++ MK Grad Program Task/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+	public GameObject followObject; // The character object for the camera to follow
+	Vector3 relativePosition; // The intended relative position of the camera from the character - Set on start
+
+    void Start()
+    {
+		relativePosition = transform.position;
+    }
+
+    void Update()
+    {
+		transform.position = followObject.transform.position + relativePosition;
+    }
+}
